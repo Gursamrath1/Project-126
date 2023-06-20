dc1ef3012f5bb8224524074e4a708a63358847e3
Gursamrath1 <74204792+Gursamrath1@users.noreply.github.com>
2023-06-20 18:36:41 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -17,9 +17,31 @@ function setup(){
     poseNet.on('pose', gotPoses);
 }
 
+function draw(){
+    image(video, 0, 0, 600, 500);
+    fill("FF0000");
+    stroke("FF0000");
+    if(scoreleftwrist>0.2){
+        circle(leftWristX, leftWristY, 20);
+        numberleftWrist=Number(leftWristY);
+        remove_decimal=floor(numberleftWrist);
+        volume=remove_decimal/500;
+        document.getElementById("volume").innerHTML = "Volume = "+ volume;
+        song.setVolume(volume);
+    }
+}
+
 function gotPoses(results){
-    if(results.length > 0){
+    if(results.length > 0);{
         console.log(results);
+        leftWristX = result[0].pose.leftWrist.x;
+        leftWristY = result[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +"leftWristY = "+leftWristY);
+
+        rightWristX = result[0].pose.rightWrist.x;
+        rightWristy = result[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +"rightWristY ="+rightWristY);
+        scoreleftwrist=results[0].pose.keypoints[9].score;
     }
 }
 
